2660acc3139746c4088765ab64e9717bd17de270
deterry69 <alfonsoterry444@gmail.com>
2026-02-23 11:09:57 +0100

    anio chino

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" CONSULTA DEL CICLO SEXAGENARIO CHINO ");
+
+        while (true) {
+            System.out.print("\nIntroduce un año: ");
+
+            if (!sc.hasNextInt()) {
+                System.out.println("¡Error! Por favor, introduce un año válido (número entero).");
+                sc.next();
+                continue;
+            }
+
+            int year = sc.nextInt();
+
+
+            calcularCiclo(year);
+        }
+    }
+
+    public static void calcularCiclo(int year) {
+        String[] elementos = {"Madera", "Fuego", "Tierra", "Metal", "Agua"};
+        String[] animales = {
+                "Rata", "Buey", "Tigre", "Conejo", "Dragón", "Serpiente",
+                "Caballo", "Oveja", "Mono", "Gallo", "Perro", "Cerdo"
+        };
+
+        int baseYear = 1984;
+        int diferencia = year - baseYear;
+
+        int indiceAnimal = Math.floorMod(diferencia, 12);
+        int indiceElemento = Math.floorMod(diferencia, 10) / 2;
+
+        System.out.printf("El año %d corresponde a: %s %s%n", year, elementos[indiceElemento], animales[indiceAnimal]);
+    }
+}
